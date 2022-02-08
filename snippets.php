30b12b38e9048db4155ac67bd0ad2c76483e9cd2
Robson Gomes <fgomes.robson@gmail.com>
2022-02-08 18:02:30 -0300

    Update snippets.php

--- snippets.php
+++ snippets.php
@@ -12,7 +12,7 @@ $client = new GuzzleHttp\Client();
 $response = $client->request('POST', 'https://oauth.hm.bb.com.br/oauth/token?grant_type=client_credentials&scope=cobrancas.boletos-requisicao', [
     'headers' => [
         'Content-Type' => 'application/x-www-form-urlencoded',
-        'Authorization' => 'Basic ZXlKcFpDSTZJamMxTjJGbUlpd2lZMjlrYVdkdlVIVmliR2xqWVdSdmNpSTZNQ3dpWTI5a2FXZHZVMjltZEhkaGNtVWlPakkyTkRNekxDSnpaWEYxWlc1amFXRnNTVzV6ZEdGc1lXTmhieUk2TVgwOmV5SnBaQ0k2SWpGaU1tRTROV010TjJFMU1TMDBZekU0TFRreVlXRXRNV1kxTnlJc0ltTnZaR2xuYjFCMVlteHBZMkZrYjNJaU9qQXNJbU52WkdsbmIxTnZablIzWVhKbElqb3lOalF6TXl3aWMyVnhkV1Z1WTJsaGJFbHVjM1JoYkdGallXOGlPakVzSW5ObGNYVmxibU5wWVd4RGNtVmtaVzVqYVdGc0lqb3hMQ0poYldKcFpXNTBaU0k2SW1odmJXOXNiMmRoWTJGdklpd2lhV0YwSWpveE5qTTVNRFE1TnpNeE5URTFmUQ=='      
+        'Authorization' => 'Basic DXlKcFpDSTZJamMxTjJGbUlpd2lZMjlrYVdkdlVIVmliR2xqWVdSdmNpSTZNQ3dpWTI5a2FXZHZVMjltZEhkaGNtVWlPakkyTkRNekxDSnpaWEYxWlc1amFXRnNTVzV6ZEdGc1lXTmhieUk2TVgwOmV5SnBaQ0k2SWpGaU1tRTROV010TjJFMU1TMDBZekU0TFRreVlXRXRNV1kxTnlJc0ltTnZaR2xuYjFCMVlteHBZMkZrYjNJaU9qQXNJbU52WkdsbmIxTnZablIzWVhKbElqb3lOalF6TXl3aWMyVnhkV1Z1WTJsaGJFbHVjM1JoYkdGallXOGlPakVzSW5ObGNYVmxibU5wWVd4RGNtVmtaVzVqYVdGc0lqb3hMQ0poYldKcFpXNTBaU0k2SW1odmJXOXNiMmRoWTJGdklpd2lhV0YwSWpveE5qTTVNRFE1TnpNeE5URTFmUQ=='      
     ],
     'verify' => false	
 ]);
